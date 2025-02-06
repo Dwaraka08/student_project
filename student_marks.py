6c763ef50f3d0037b0def7de7d17ae4e74ca60d9
Dwaraka08 <161102021+Dwaraka08@users.noreply.github.com>
2025-02-06 14:32:15 +0530

    Add files via upload

---
+++ student_marks.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import dill
+import numpy as np
+
+st.title("Student Marks Prediction")
+
+
+Hours=st.text_input("Enter the number of hours you study:")
+
+with open(r"C:\Users\my pc\s_project.dill", "rb") as f:
+    model = dill.load(f)
+
+if st.button("prediction"):
+    data=[[Hours]]
+    data_array=np.array(data)
+    prediction=model.predict(data_array)
+    st.write(prediction)
